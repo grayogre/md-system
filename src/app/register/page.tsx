c72a803e92b863f8c9fdb767d53709488d2cb350
grayogre <grayogre@gmail.com>
2023-07-13 12:15:34 +0900

    入力フィールドの参照にuseRefを使うようにした。 バリデーションの結果をフィールドごとに分けた。

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import axios from '../axios'
 import Frame from '../../components/Frame'
@@ -20,20 +20,22 @@ export default function Home() {
     }
   }
  
-  const [result, setResult] = useState(defaultResult)
-  const [showPass, setShowPass] = useState(false)
+  const namefld = useRef<HTMLInputElement>(null)
+  const emailfld = useRef<HTMLInputElement>(null)
+  const passfld = useRef<HTMLInputElement>(null)
+  const [nameErr, setNameErr] = useState<string[]>([])
+  const [emailErr, setEmailErr] = useState<string[]>([])
+  const [passErr, setPassErr] = useState<string[]>([])
   const [error, setError] = useState('')
+  const [showPass, setShowPass] = useState(false)
   const router = useRouter()
 
   const onClick = async () => {
-    const namefld = document.getElementById('name') as HTMLInputElement
-    const emailfld = document.getElementById('email') as HTMLInputElement
-    const passfld = document.getElementById('password') as HTMLInputElement
     await axios.get('/sanctum/csrf-cookie')
     axios.post('/api/register', {
-      name: namefld?.value,
-      email: emailfld?.value,
-      password: passfld?.value
+      name:namefld.current?.value,
+      email: emailfld.current?.value,
+      password: passfld.current?.value
     },)
       .then((res) => {
         console.log('reg', res)
@@ -43,10 +45,14 @@ export default function Home() {
         console.log(err)
         if (err.code === 'ERR_BAD_REQUEST') {
           setError('')
-          setResult(err.response.data)
+          setNameErr(err.response.data.errors?.name)
+          setEmailErr(err.response.data.errors?.email)
+          setPassErr(err.response.data.errors?.password)
         } else {
           setError(err.message)
-          setResult(defaultResult)
+          setNameErr([])
+          setEmailErr([])
+          setPassErr([])
         }
       })
   } 
@@ -62,19 +68,19 @@ export default function Home() {
         <form method="post">
           <div className="frex frex-row">
             <label className="label-primary" htmlFor="name">ニックネーム</label>
-            <input className="input-primary" type="text" id="name" placeholder="ニックネーム" />
-            <Errors className="block" messages={result.errors?.name ?? []} />
+            <input className="input-primary" type="text" id="name" placeholder="ニックネーム" ref={namefld}/>
+            <Errors className="block" messages={nameErr ?? []} />
             <label className="label-primary" htmlFor="email">Eメール</label>
-            <input className="input-primary" type="email" id="email" placeholder="aaa@bbb.com" />
-            <Errors className="block" messages={result.errors?.email ?? []} />
+            <input className="input-primary" type="email" id="email" placeholder="aaa@bbb.com" ref={emailfld} />
+            <Errors className="block" messages={emailErr ?? []} />
             <label className="label-primary" htmlFor="password">パスワード</label>
             <div className="flex">
-              <input className="input-primary" type={showPass ? "text" : "password"} id="password" placeholder="パスワード" />
+              <input className="input-primary" type={showPass ? "text" : "password"} id="password" placeholder="パスワード" ref={passfld}/>
               <span className="mb-2" onClick={toggleShowPass}>
                 {showPass ? <OpenEye /> : <CloseEye /> }
               </span>
             </div>
-            <Errors className="block" messages={result.errors?.password ?? []} />
+            <Errors className="block" messages={passErr ?? []} />
             <Errors className="block" messages={error !== '' ? [error] : []} />
           </div>
           <div>
