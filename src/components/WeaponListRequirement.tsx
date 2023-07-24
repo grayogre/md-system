28a13d59d47de0eccd21c20b69e85f8e2684ce8e
grayogre <grayogre@gmail.com>
2023-07-24 13:57:11 +0900

    未ログイン時の処理を追加 見出しサイズを変更

--- src/components/WeaponListRequirement.tsx
+++ src/components/WeaponListRequirement.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import axios from '../app/axios'
 import Errors from './Errors'
 
@@ -17,6 +18,8 @@ export default function WeaponListReqirement(props: {setList: (value:any[]) => v
   const [errMsg, setErrMsg] = useState('')
 
   const setList = props.setList
+
+  const router = useRouter()
 
   const doQuery = () => {
     setErrMsg('');
@@ -37,10 +40,13 @@ export default function WeaponListReqirement(props: {setList: (value:any[]) => v
       setList(res.data)
     }).catch((err) => {
       console.log('error:', err)
-      if (err.response.status === 429) {
-         setErrMsg("検索頻度が多すぎます。しばらくお待ち下さい。")
+      if (err.response.status === 401) {
+        setErrMsg("ログインしていません。")
+        router.push('/login')
+      } else if (err.response.status === 429) {
+        setErrMsg("検索頻度が多すぎます。しばらくお待ち下さい。")
       } else {
-         setErrMsg(err.response.statusText)
+        setErrMsg(err.response.statusText)
       }
     })
   }
@@ -64,12 +70,12 @@ export default function WeaponListReqirement(props: {setList: (value:any[]) => v
 
   return (
     <form>
-      <fieldset className="flex md:flex-row flex-col justify-start border border-solid border-black p-3">
+      <fieldset className="flex md:flex-row flex-col justify-start border border-solid border-black px-3 py-1">
         <legend>絞り込み条件</legend>
-        <label className="ml-5" >武器名
+        <label className="ml-5"><span className="font-xs font-bold">武器名</span><br />
           <input type="text" className="border border-solid border-black px-1 w-32" value={namePart} onChange={onChangeName} />
         </label>
-        <label className="ml-5">種別
+        <label className="ml-5"><span className="font-xs font-bold">種別</span><br />
           <select onChange={onChangeType} value={attackType} className="border border-solid border-black">
             <option value="-1">(未指定)</option>
             <option value="0">射撃</option>
@@ -78,31 +84,31 @@ export default function WeaponListReqirement(props: {setList: (value:any[]) => v
           </select>
         </label>
         
-        <label className="ml-5">
+        <label className="ml-5 mt-6">
           <input type="checkbox"  checked={myWeapon} onChange={(e) => onChangeBool(e, setMyWeapon)} />
           自分で登録
         </label>
-        <label className="ml-5">
+        <label className="ml-5 mt-6">
           <input type="checkbox" checked={headMount} onChange={(e) => onChangeBool(e, setHeadMount)} />
           頭装備
         </label>
-        <label className="ml-5">
+        <label className="ml-5 mt-6">
           <input type="checkbox" checked={handMount} onChange={(e) => onChangeBool(e, setHandMount)} />
           手装備
         </label>
-        <label className="ml-5">
+        <label className="ml-5 mt-6">
           <input type="checkbox" checked={armMount} onChange={(e) => onChangeBool(e, setArmMount)} />
           腕装備
         </label>
-        <label className="ml-5">
+        <label className="ml-5 mt-6">
           <input type="checkbox" checked={shoulderMount} onChange={(e) => onChangeBool(e, setShoulderMount)} />
           肩装備
         </label>
-        <label className="ml-5">
+        <label className="ml-5 mt-6">
           <input type="checkbox" checked={torsoMount} onChange={(e) => onChangeBool(e, setTorsoMount)} />
           胴装備
         </label>
-        <label className="ml-5">
+        <label className="ml-5 mt-6">
           <input type="checkbox" checked={legMount} onChange={(e) => onChangeBool(e, setLegMount)} />
           脚装備
         </label>
